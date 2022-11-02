6f7fa53f443c78ddf0b2221a7423cc5568a62c11
haesa <xdhaesa@gmail.com>
2022-11-02 21:03:03 +0900

    chore: 사용자 식별과 검증 로직 합치기

--- index.php
+++ index.php
@@ -1,33 +1,66 @@
 <?php
-session_start();
+  session_start();
+  $massage = "";
+  if(isset($_POST['username'], $_POST['password'])) {
+    include "./dbconn.php";
+
+    if(preg_match("/admin|select|Union|by|having|substring|substr|from|char|delay|0x|hex|asc|desc|ascii/i", $_POST["username"])) exit("~~filtering~~");
+    if(preg_match("/admin|select|Union|by|having|substring|substr|from|char|delay|0x|hex|asc|desc|ascii/i", $_POST["password"])) exit("~~filtering~~");
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $query = "select * from users where id='{$username}' and pw='{$password}';";
+    $record = mysqli_query($conn ,$query);
+    if(!$record) 
+      die(mysqli_error($conn));
+    
+    $result = mysqli_fetch_array($record);
+
+    if($result) {
+      $massage = "query: true";
+      if($result["pw"] == $password) {
+        $massage = "";
+        $_SESSION["username"] = $result["id"];
+        if($result["id"] == "admin")
+          $_SESSION["flag"] = "POX{POWER_OF_XX_2022}";  
+      }
+    } 
+  }
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <link rel="stylesheet" href="style.css">
-    <title>Blind SQL Injection</title>
+    <title>Login</title>
   </head>
   <body>
     <div id="content">
       <?php
-        if (isset($_SESSION['userId'])) {
-          echo "<div id='logout'>";
-          echo "<h2>{$_SESSION['userId']}님 환영합니다!</h1>";
-          if($_SESSION["userId"] == "admin") echo "<p>POX{POWER_OF_XX_2022}</p>";
-          echo "<button onclick='location.href = `logout.php`;'>로그아웃</button>";
-          echo "</div>";
-        } else {
+        if(isset($_SESSION['username'])) { 
       ?>
-      <div id="login">
-        <form id="login_form" action="login.php" method="POST">
-          <input type="text" name="userId" placeholder="아이디" required>
-          <input type="password" name="userPw" placeholder="비밀번호" required>
-          <input type="submit" value="로그인">
-        </form>
-      </div>
+          <div id='logout'>
+          <?php 
+            echo "<h2>{$_SESSION['username']}님 환영합니다!</h2>";
+            if(isset($_SESSION["flag"])) echo "<p>{$_SESSION['flag']}</p>";
+          ?>
+            <button onclick='location.href = `logout.php`;'>로그아웃</button>
+          </div>
       <?php 
+        } else { 
+      ?>
+        <div id="login">
+          <form id="login_form" action="index.php" method="POST">
+            <input type="text" name="username" placeholder="아이디" required>
+            <input type="password" name="password" placeholder="비밀번호" required>
+            <input type="submit" value="로그인">
+          </form>
+        </div>
+      <?php
         }
+        echo "<p class='query'>{$massage}</p>";
       ?>
     </div>
   </body>
 </html>
+<!-- hint: guest / guest -->
